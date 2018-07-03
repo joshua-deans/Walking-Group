b8ab427bfe8582bc2f4aa5f0b717cda6675bc890
ktorsen <ktorsen@sfu.ca>
2018-07-02 22:27:56 -0700

    Login page can verify user and let user get into next step.

--- app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/LoginActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/LoginActivity.java
@@ -1,15 +1,11 @@
 package ca.cmpt276.walkinggroupindigo.walkinggroup.app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import java.util.List;
 
 import ca.cmpt276.walkinggroupindigo.walkinggroup.R;
 import ca.cmpt276.walkinggroupindigo.walkinggroup.dataobjects.User;
@@ -21,36 +17,45 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginScreen";
     private WGServerProxy proxy;
 
-//    User user;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-        String apikey = null;
-        proxy = ProxyBuilder.getProxy(getString(R.string.apikey),null);
-//        user = User.getInstance();
-
+        getApiKey();
         setUpLoginButton();
         setUpSignUpButton();
     }
 
-    private void setUpLoginButton() {
-        User user = new User();
-        EditText emailEditTxt = findViewById(R.id.emailEdit);
-        String userEamil;
-        userEamil = emailEditTxt.getText().toString();
-        EditText passEditTxt = findViewById(R.id.passEdit);
-        String userPass;
-        userPass = passEditTxt.getText().toString();
-        user.setEmail(userEamil);
-        user.setPassword(userPass);
+    private void getApiKey() {
+        String apiKey = getString(R.string.apikey);
+        proxy = ProxyBuilder.getProxy(apiKey,null);
 
-        ProxyBuilder.setOnTokenReceiveCallback(token -> onReceiveToken(token));
+    }
+
+    private void setUpLoginButton() {
+        Button button = findViewById(R.id.login_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+            }
+        });
+    }
+
+    private void LoginUser() {
+        User user = new User();
+        String userEmail = getInputText(R.id.emailEdit);
+        String userPass = getInputText(R.id.passEdit);
+        user.setEmail(userEmail);
+        user.setPassword(userPass);
         Call<Void> caller = proxy.login(user);
-        ProxyBuilder.callProxy(LoginActivity.this, caller, returnedUser -> response(returnedUser));
+        // Need to go to Manage account activity
+    }
+
+    private String getInputText(int id){
+        EditText text = findViewById(id);
+        return text.getText().toString();
     }
 
     private void setUpSignUpButton() {
@@ -63,25 +68,5 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-    }
-    private void notifyUserViaLogAndToast(String message) {
-        Log.w(TAG, message);
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
-    private void response(Void returnedNothing) {
-        notifyUserViaLogAndToast("Server replied to login request (no content was expected).");
-    }
-
-    private void response(List<User> returnedUsers) {
-        notifyUserViaLogAndToast("Got list of " + returnedUsers.size() + " users! See logcat.");
-        Log.w(TAG, "All Users:");
-        for (User user : returnedUsers) {
-            Log.w(TAG, "    User: " + user.toString());
-        }
-    }
-    private void onReceiveToken(String token) {
-        // Replace the current proxy with one that uses the token!
-        Log.w(TAG, "   --> NOW HAVE TOKEN: " + token);
-        proxy = ProxyBuilder.getProxy(getString(R.string.apikey), token);
     }
 }
