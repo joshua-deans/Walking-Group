757840d9120112623081ddd7d03bcc4f6608eff7
cheng <zya46@sfu.ca>
2018-07-04 23:28:20 -0700

    commit to update

---
+++ app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/DashboardActivity.java
@@ -0,0 +1,115 @@
+package ca.cmpt276.walkinggroupindigo.walkinggroup.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ca.cmpt276.walkinggroupindigo.walkinggroup.R;
+import ca.cmpt276.walkinggroupindigo.walkinggroup.dataobjects.User;
+
+import static ca.cmpt276.walkinggroupindigo.walkinggroup.app.LoginActivity.LOG_IN_KEY;
+import static ca.cmpt276.walkinggroupindigo.walkinggroup.app.LoginActivity.LOG_IN_SAVE_KEY;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private User mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        mUser = User.getInstance();
+
+        Toast.makeText(DashboardActivity.this, mUser.toString(), Toast.LENGTH_LONG).show();
+
+        createGreeting();
+        setupMapButton();
+        setupMonitorButton();
+        setupGroupButton();
+    }
+
+    private void createGreeting() {
+        TextView greeting = (TextView) findViewById(R.id.welcome_user);
+        greeting.setText(String.format("Welcome %s!", mUser.getName()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_dashboard, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logOutButton:
+                // TODO: Implement log out
+                Toast.makeText(DashboardActivity.this, R.string.logged_out, Toast.LENGTH_SHORT).show();
+                logUserOut();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void logUserOut() {
+        Context context = DashboardActivity.this;
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                LOG_IN_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(LOG_IN_SAVE_KEY, "");
+        editor.apply();
+
+        Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void setupGroupButton() {
+        Button groupButton = (Button) findViewById(R.id.manage_group);
+        groupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Add group
+                Toast.makeText(DashboardActivity.this, "Groups is not ready yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupMonitorButton() {
+        Button monitorButton = (Button) findViewById(R.id.manage_monitoring);
+        monitorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Add monitoring
+                Toast.makeText(DashboardActivity.this, "Monitoring is not ready yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupMapButton() {
+        Button mapButton = (Button) findViewById(R.id.viewMapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/SignUpActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/SignUpActivity.java
@@ -2,12 +2,35 @@ package ca.cmpt276.walkinggroupindigo.walkinggroup.app;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import ca.cmpt276.walkinggroupindigo.walkinggroup.R;
+import ca.cmpt276.walkinggroupindigo.walkinggroup.dataobjects.User;
+import ca.cmpt276.walkinggroupindigo.walkinggroup.proxy.ProxyBuilder;
+import ca.cmpt276.walkinggroupindigo.walkinggroup.proxy.WGServerProxy;
+import retrofit2.Call;
+
+/*
+ * Create a user based on the provided information.
+ * Name, Email, password, birth date, address, cellphone
+ * home phone, grade, teacher name, and emergency contact information
+ */
 
 public class SignUpActivity extends AppCompatActivity {
+    private WGServerProxy proxy;
+    private User user;
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, SignUpActivity.class);
@@ -17,5 +40,120 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getApiKey();
+        setupSignUpButton();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_signup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logInButton:
+                finish();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    
+    private void getApiKey() {
+        String apiKey = getString(R.string.apikey);
+        proxy = ProxyBuilder.getProxy(apiKey,null);
+
+    }
+
+    // When clicked, a new user will create
+    private void setupSignUpButton() {
+        Button button = findViewById(R.id.signup_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = getTextInformation(R.id.user_name);
+                String email = getTextInformation(R.id.email_information);
+                String password = getTextInformation(R.id.create_password);
+                String confirm_password = getTextInformation(R.id.confirm_password);
+                CreateUser(name, email, password, confirm_password);
+            }
+        });
+    }
+
+    // Based on the provided view_id, return the filled information
+    private String getTextInformation(int view_id) {
+        EditText text = findViewById(view_id);
+        return text.getText().toString();
+    }
+
+    // Create a user based on the information provided
+    private void CreateUser(String name, String email, String password, String confirm_password) {
+        if (name == null || name.matches("")) {
+            Toast.makeText(SignUpActivity.this, R.string.please_enter_name, Toast.LENGTH_SHORT).show();
+        } else if (email == null || email.matches("")) {
+            Toast.makeText(SignUpActivity.this, R.string.please_enter_email, Toast.LENGTH_SHORT).show();
+        } else if (password.length() == 0) {
+            Toast.makeText(SignUpActivity.this, R.string.password_empty, Toast.LENGTH_SHORT).show();
+        } else if (!(password.equals(confirm_password))) {
+            Toast.makeText(SignUpActivity.this, R.string.passwords_dont_match, Toast.LENGTH_SHORT).show();
+        } else {
+            user = new User();
+            user.setName(name);
+            user.setEmail(email);
+            user.setPassword(password);
+            insertIntoServer(user);
+        }
+    }
+
+    // Insert the successfully created user into the server
+    private void insertIntoServer(User user) {
+
+        Call<List<User>> usersCaller = proxy.getUsers();
+        List<User> existingUsers = new ArrayList<>();
+
+        ProxyBuilder.callProxy(SignUpActivity.this, usersCaller, returnedUsers -> {
+            existingUsers.addAll(returnedUsers);
+        });
+
+        if(isDuplicated(existingUsers, user)) {
+            Toast.makeText(SignUpActivity.this,
+                    R.string.duplicated_email,
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Call<User> caller = proxy.createUser(user);
+        ProxyBuilder.callProxy(SignUpActivity.this, caller, returnedUser -> successfulSignUp(returnedUser));
+            // Need to go to Account monitor activity
+//        }
+
+    }
+
+    private boolean isDuplicated(List<User> users, User user) {
+        for (User aUser : users) {
+            if (aUser.getEmail().equalsIgnoreCase(user.getEmail())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void successfulSignUp(User returnedUser) {
+        if (returnedUser != null) {
+            Toast.makeText(SignUpActivity.this,
+                    R.string.success_sign_up,
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(SignUpActivity.this,
+                    R.string.no_success_sign_up,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
