676cd96046d84158c2d216f3bf50f82e708a455d
ktorsen <aabulimiti@xmatters.com>
2018-07-04 20:48:17 -0700

    SignUpActicity duplicated email problem solved.

--- app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/SignUpActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/SignUpActivity.java
@@ -12,7 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ca.cmpt276.walkinggroupindigo.walkinggroup.R;
 import ca.cmpt276.walkinggroupindigo.walkinggroup.dataobjects.User;
@@ -112,12 +114,34 @@ public class SignUpActivity extends AppCompatActivity {
 
     // Insert the successfully created user into the server
     private void insertIntoServer(User user) {
-        Call<List<User>> users = proxy.getUsers();
+
+        Call<List<User>> usersCaller = proxy.getUsers();
+        List<User> existingUsers = new ArrayList<>();
+
+        ProxyBuilder.callProxy(SignUpActivity.this, usersCaller, returnedUsers -> {
+            existingUsers.addAll(returnedUsers);
+        });
+
+        if(isDuplicated(existingUsers, user)) {
+            Toast.makeText(SignUpActivity.this,
+                    R.string.duplicated_email,
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
         Call<User> caller = proxy.createUser(user);
         ProxyBuilder.callProxy(SignUpActivity.this, caller, returnedUser -> successfulSignUp(returnedUser));
             // Need to go to Account monitor activity
 //        }
 
+    }
+
+    private boolean isDuplicated(List<User> users, User user) {
+        for (User aUser : users) {
+            if (aUser.getEmail().equalsIgnoreCase(user.getEmail())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void successfulSignUp(User returnedUser) {
