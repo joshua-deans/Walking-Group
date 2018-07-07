78e6ca0ca4d0cf67f9669df8bc0b996573e884e3
aka125 <aka125@sfu.ca>
2018-07-06 19:29:26 -0700

    Adding functionality to remove monitored user from groups.

---
+++ app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/fragments/RemoveMessageFragment.java
@@ -0,0 +1,35 @@
+package ca.cmpt276.walkinggroupindigo.walkinggroup.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import ca.cmpt276.walkinggroupindigo.walkinggroup.R;
+
+public class RemoveMessageFragment extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.monitoring_user_removal_message, null);
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        };
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("Removing a user")
+                .setView(v)
+                .setPositiveButton(android.R.string.ok, listener)
+                .setNegativeButton(android.R.string.cancel, listener)
+                .create();
+    }
+}
