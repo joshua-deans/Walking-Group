e444cdcd569a2b8699b96790e124071c4b608c10
jdeans <joshua.deans21@gmail.com>
2018-07-19 22:47:28 -0700

    Updates to refactoring

--- app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/ManageMonitoring.java
+++ app/src/main/java/ca/cmpt276/walkinggroupindigo/walkinggroup/app/ManageMonitoring.java
@@ -3,7 +3,6 @@ package ca.cmpt276.walkinggroupindigo.walkinggroup.app;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -32,10 +31,6 @@ import ca.cmpt276.walkinggroupindigo.walkinggroup.proxy.ProxyBuilder;
 import ca.cmpt276.walkinggroupindigo.walkinggroup.proxy.ProxyFunctions;
 import ca.cmpt276.walkinggroupindigo.walkinggroup.proxy.WGServerProxy;
 import retrofit2.Call;
-
-import static ca.cmpt276.walkinggroupindigo.walkinggroup.app.LoginActivity.LOG_IN_KEY;
-import static ca.cmpt276.walkinggroupindigo.walkinggroup.app.LoginActivity.LOG_IN_SAVE_KEY;
-import static ca.cmpt276.walkinggroupindigo.walkinggroup.app.LoginActivity.LOG_IN_SAVE_TOKEN;
 
 public class ManageMonitoring extends AppCompatActivity {
 
@@ -168,21 +163,6 @@ public class ManageMonitoring extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
-    }
-
-    private void logUserOut() {
-        Context context = ManageMonitoring.this;
-        SharedPreferences sharedPref = context.getSharedPreferences(
-                LOG_IN_KEY, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(LOG_IN_SAVE_KEY, "");
-        editor.putString(LOG_IN_SAVE_TOKEN, "");
-        editor.apply();
-
-        Intent intent = new Intent(ManageMonitoring.this, LoginActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
     }
 
     private void populateMonitorsUser() {
